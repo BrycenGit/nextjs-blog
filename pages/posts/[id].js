4d817bf3c1e81554eec894d46d3148ef0bf265cb
Brycen Bartolome <brycenbartolome@gmail.com>
2021-01-13 00:29:56 -0800

    add date fns library

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,24 @@
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -15,4 +32,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
